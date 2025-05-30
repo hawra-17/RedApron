5e6dd07ec05e7df874b43004b0ca4e569beff91c
Hawra Alaradi <123960405+hawra-17@users.noreply.github.com>
2025-05-30 16:28:32 +0300

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import "../global.css";
+
+const Home = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-cyan-600 text-xl">Welcome, Hawra</Text>
+    </View>
+  );
+};
+
+export default Home;
